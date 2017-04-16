4932400771088f6843d8d06265298c23892e878e
mntalateyya <mnurulho@andrew.cmu.edu>
2017-04-16 22:53:39 +0300

    All remaining symmetries

--- experimental/frieze.py
+++ experimental/frieze.py
@@ -1,33 +1,66 @@
 import Tkinter
 
+
 def repeat(cw, ch, r, s, (x, y)):
     # canvas width, canvas height, width:height ratio of cell,
     # symmetry type, (x,y) of mouse
-    x %= int(ch * r)
     coords = []
-    if s == 0:
+    if s == 0:  # pmm2
+        x %= int(ch * r)
         coords = [(x, y)]
-        coords += [(x, ch - y-1)]
-        coords += [(int(ch * r - i[0]-1), i[1]) for i in coords[-2:]]
+        coords += [(x, ch - y - 1)]
+        coords += [(int(ch * r - i[0] - 1), i[1]) for i in coords[-2:]]
         while coords[-1][0] < cw:
-            coords += [(int(i[0]+ch*r), i[1]) for i in coords[-4:]]
-    elif s==1:
+            coords += [(int(i[0] + ch * r), i[1]) for i in coords[-4:]]
+    elif s == 1:  # pma2
+        x %= int(ch * r)
         coords = [(x, y)]
-        coords += [(int(ch*r-x-1), ch - y - 1)]
+        coords += [(int(ch * r - x - 1), ch - y - 1)]
         while coords[-1][0] < cw:
-            coords += [(int(ch*r+i[0]), ch - i[1] - 1) for i in coords[-2:]]
+            coords += [(int(ch * r + i[0]), ch - i[1] - 1) for i in coords[-2:]]
+    elif s == 2:  # p112
+        x %= int(ch * r)
+        coords = [(x, y)]
+        coords += [(int(2 * ch * r - x) - 1, int(ch - y) - 1)]
+        while coords[-1][0] < cw:
+            coords += [(int(i[0] + 2 * ch * r), i[1]) for i in coords[-2:]]
+    elif s == 3:  # p1m1
+        x %= int(0.5 * ch * r)
+        coords = [(x, y)]
+        coords += [(x, ch - y - 1)]
+        while coords[-1][0] < cw:
+            coords += [(int(i[0] + 0.5 * ch * r), i[1]) for i in coords[-2:]]
+    elif s==4:  #pm11
+        x %= int(ch * r)
+        coords = [(x, y)]
+        coords += [(int(2*ch*r-x)-1, y)]
+        while coords[-1][0] < cw:
+            coords += [(int(i[0] + 2* ch * r), i[1]) for i in coords[-2:]]
+    elif s==5:  # p111
+        x %= int(ch * r)
+        coords = [(x, y)]
+        while coords[-1][0] < cw:
+            coords += [(coords[-1][0]+int(ch*r), coords[-1][1])]
+    elif s==6: # p1a1
+        x %= int(ch * r)
+        coords = [(x, y)]
+        while coords[-1][0] < cw:
+            coords += [(coords[-1][0]+int(ch*r), ch-coords[-1][1]-1)]
+
     return coords
+
 
 def drawcircle(e):
     global c
-    coords = repeat(600,60,1.5,1,(e.x,e.y))
+    coords = repeat(600, 60, 1, 6, (e.x, e.y))
     for i in coords:
-        c.create_oval(i[0]-2,i[1]-2,i[0]+2,i[1]+2, fill='blue')
+        c.create_oval(i[0] - 2, i[1] - 2, i[0] + 2, i[1] + 2, fill='blue')
 
-# print repeat(30, 3, 1, 1, (2,0))
 
+print repeat(30, 3, 1, 6, (2, 2))
+# '''
 wnd = Tkinter.Tk()
-c = Tkinter.Canvas(wnd,width=600, height=60)
+c = Tkinter.Canvas(wnd, width=600, height=60)
 c.pack()
-c.bind('<Button-1>',drawcircle)
-wnd.mainloop()
+c.bind('<Button-1>', drawcircle)
+wnd.mainloop()  # '''
